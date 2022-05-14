71488731886a63c6b541e887373ad097934ed9c7
kamal1198 <kaur.preetkamal1198@gmail.com>
2022-05-14 16:35:03 -0400

    third update

--- assets/css/script.js
+++ assets/css/script.js
@@ -1,52 +1,82 @@
 // use start button to load first question
-var startbutton=document.getElementById("startbtn");
-var quiz=document.getElementById("quiz");
-startbutton.addEventListener("click",function(){
-var promt=document.getElementById("promt");
-promt.setAttribute("class","hide")
-quiz.removeAttribute("class");
-buildquestion()
+var startbutton = document.getElementById("startbtn");
+var quiz = document.getElementById("quiz");
+startbutton.addEventListener("click", function () {
+    var promt = document.getElementById("promt");
+
+
+
+    promt.setAttribute("class", "hide")
+    quiz.removeAttribute("class");
+    buildquestion()
 })
-var question=[
+var question = [
     {
-        text:"question 1",
-        choices:["a","b","c","d"],
-        answer:"a"
+        text: "question 1",
+        choices: ["a", "b", "c", "d"],
+        answer: "a"
     },
     {
-        text:"question 2",
-        choices:["a","b","c","d"],
-        answer:"a"
+        text: "question 2",
+        choices: ["a", "b", "c", "d"],
+        answer: "a"
     },
     {
-        text:"question 3",
-        choices:["a","b","c","d"],
-        answer:"a"
+        text: "question 3",
+        choices: ["a", "b", "c", "d"],
+        answer: "a"
     },
-]
-function buildquestion(){
-    var questiontext=document.getElementById("questiontext");
-    questiontext.textContent=question[0].text;
-    var buttonbox=document.getElementById("buttonbox");
-    question[0].choices.forEach(function(choice){
-var choicebtn=document.createElement("button");
-choicebtn.textContent=choice;
-choicebtn.setAttribute("value",choice)
-choicebtn.onclick=function(){
-    if (this.value===question[0].answer){
-        console.log("correct")
-    } else {
-        console.log("wrong")
-    }
+    {
+        text: "question 4",
+        choices: ["a", "b", "c", "d"],
+        answer: "a"
+    },
+    {
+        text: "question 5",
+        choices: ["a", "b", "c", "d"],
+        answer: "a"
+    },
+];
 
+function buildquestion() {
+    var questiontext = document.getElementById("questiontext");
+    questiontext.textContent = question[0].text;
+    var buttonbox = document.getElementById("buttonbox");
+    question[0].choices.forEach(function (choice) {
+        var choicebtn = document.createElement("button");
+        choicebtn.textContent = choice;
+        choicebtn.setAttribute("value", choice)
+        choicebtn.onclick = function () {
+            if (this.value === question[0].answer) {
+                console.log("correct");
+            } else {
+                console.log("wrong");
+                // timer-=10;   
+            }
+        }
+        
+        var time = question.length * 15;
+        const timerInt = setInterval(function () {
+            const timerEl = document.getElementById('timer');
+            timerEl.innerText = time--;
+            if (time <= 0) { 
+                time=0;
+                clearInterval(timerInt); 
+                // TODO display endscreen
+        
+
+            }
+        }, 1000);
+
+        
+  
+  
+     
+
+        buttonbox.appendChild(choicebtn)
+    });
 }
 
 
 
 
-
-
-
-buttonbox.appendChild(choicebtn)
-    })
-}
